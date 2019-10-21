fc1ce033a986189695b8241192ed3ed4e8f9aa04
SangMin <min112402@gmail.com>
2019-10-21 23:28:33 +0900

    step3

---
+++ src/app/mock-heros.ts
@@ -0,0 +1,14 @@
+import {Hero} from './hero';
+
+export const HEROES: Hero[] =[
+    { id: 11, name: 'Iron Man'},
+    { id: 12, name: 'Narco'},
+    { id: 13, name: 'Batman'},
+    { id: 14, name: 'Dynama'},
+    { id: 15, name: 'Black Panther'},
+    { id: 16, name: 'Thor'},
+    { id: 17, name: 'Hulk'},
+    { id: 18, name: 'Captain America'},
+    { id: 19, name: 'Black Widow'},
+    { id: 20, name: 'Hawkeye'},
+]
